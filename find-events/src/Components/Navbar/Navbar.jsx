f236b5a1576602df6ff55cf3202f87fb4cbfe14a
apanga01 <pallyssam@gmail.com>
2024-07-27 19:40:16 -0400

    updated Navbar

--- find-events/src/Components/Navbar/Navbar.jsx
+++ find-events/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <span className="logo-text">Event <span className="highlight">Finder</span></span>
+      </div>
+      <button className="search-button">Search Events</button>
+    </nav>
   )
 }
 
