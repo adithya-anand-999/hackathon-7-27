19a4c745c586c11463f8e4cec931580c7a66b944
apanga01 <pallyssam@gmail.com>
2024-07-28 02:06:04 -0400

    updated use AI button

--- find-events/src/Components/Navbar/Navbar.jsx
+++ find-events/src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import './Navbar.css';
-import { useNavigate } from 'react-router-dom';
 
-const Navbar = () => {
-  const navigate = useNavigate();
-
-  const handleAIButtonClick = () => {
-    navigate('/ai');
-  };
-
+const Navbar = ({ scrollToBase }) => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
@@ -16,12 +9,10 @@ const Navbar = () => {
       </div>
       <div className="navbar-buttons">
         <button className="search-button">Search Events</button>
-        {/* <button className="ai-button" >Use AI</button> */}
-        <button className="ai-button" onClick={handleAIButtonClick}>Use AI</button>
+        <button className="ai-button" onClick={scrollToBase}>Use AI</button>
       </div>
     </nav>
   );
-}
+};
 
-
-export default Navbar
+export default Navbar;
