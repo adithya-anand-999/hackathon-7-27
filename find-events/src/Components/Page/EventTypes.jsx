448edf2ea5e69324e2946ff7d8f7d55cd8df74e3
ayeshm3 <129223497+ayeshm3@users.noreply.github.com>
2024-07-27 19:00:49 -0700

    completed search bar, heading and pictures, as well as resource types

--- find-events/src/Components/Page/EventTypes.jsx
+++ find-events/src/Components/Page/EventTypes.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './EventTypes.css';
 
 const eventTypes = [
-  { name: 'MUSIC', image: '../../Assets/music.jpg' },
-  { name: 'FESTIVITIES', image: '../../Assets/festivities.jpg' },
-  { name: 'FUNDRAISER', image: '../../Assets/fundraiser.jpg' },
-  { name: 'CONCERT', image: '../../Assets/concert.jpg' },
-  { name: 'NETWORKING', image: '../../Assets/social.jpg' }
+  { name: 'MUSIC', image: require('../../Assets/music.jpg') },
+  { name: 'FESTIVITIES', image: require('../../Assets/festivities.jpg') },
+  { name: 'FUNDRAISER', image: require('../../Assets/fundraiser.jpg') },
+  { name: 'CONCERT', image: require('../../Assets/concert.jpg') },
+  { name: 'NETWORKING', image: require('../../Assets/social.jpg') }
 ];
 
 const EventTypes = () => {
@@ -17,7 +17,7 @@ const EventTypes = () => {
         {eventTypes.map((type, index) => (
           <div className="event-type" key={index}>
             <div className="image-placeholder">
-              {/* Placeholder for image */}
+              <img src={type.image} alt={type.name} className="event-image" />
             </div>
             <p>{type.name}</p>
           </div>
